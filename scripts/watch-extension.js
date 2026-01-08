1f7246a65f6295212e56743278d3e9a76edfa895
josemqu <mailjmq@gmail.com>
2026-01-08 00:05:30 -0300

    pnpm watch-extension script added

--- scripts/watch-extension.js
+++ scripts/watch-extension.js
@@ -19,6 +19,15 @@ const server = http.createServer((req, res) => {
   }
 });
 
+server.on('error', (e) => {
+  if (e.code === 'EADDRINUSE') {
+    console.error(`\x1b[31m[Watcher] Error: Port ${PORT} is already in use.\x1b[0m`);
+    console.error(`\x1b[31m[Watcher] Please stop any other running instances of this script or kill the process using port ${PORT}:\x1b[0m`);
+    console.error(`\x1b[33m          lsof -i :${PORT} | grep LISTEN | awk '{print $2}' | xargs kill -9\x1b[0m`);
+    process.exit(1);
+  }
+});
+
 server.listen(PORT, () => {
   console.log(`\x1b[32m[Watcher] Server listening on http://localhost:${PORT}/timestamp\x1b[0m`);
   console.log(`\x1b[34m[Watcher] Watching directory: ${WATCH_DIR}\x1b[0m`);
